c2c3e80fd6f6f0ed6742d716c444dd597944f330
Murad Musali <Murad Musali@MM>
2022-04-11 21:34:43 +0400

    Add project files.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        //Encapsulation
+        public static void Add(Product product)
+        {
+            Console.WriteLine($"{product.ProductName} adli mehsul elave olundu!");
+        }
+
+        public static void Update(Product product)
+        {
+            Console.WriteLine($"{product.ProductName} adli mehsul yenilendi!");
+        }
+
+        public static void Delete(Product product)
+        {
+            Console.WriteLine($"{product.ProductName} adli mehsul silindi!");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,25 @@
+﻿using OOP1;
+
+
+
+Product productOne = new()
+{
+    Id = 1,
+    CategoryId = 1,
+    ProductName = "Iphone",
+    UnitPrice = 1200,
+    UnitsInStock = 5
+};
+
+Product productTwo = new()
+{
+    Id = 2,
+    CategoryId = 1,
+    ProductName = "Samsung",
+    UnitPrice = 700,
+    UnitsInStock = 10
+};
+
+ProductManager.Add(productOne);
+ProductManager.Update(productTwo);
+ProductManager.Delete(productOne);
